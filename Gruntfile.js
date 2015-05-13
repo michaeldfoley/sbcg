c2a115bc228d75d6888321a1af1ab67414969dfb
Michael Foley <michaeldfoley@gmail.com>
2015-05-13 16:07:03 -0400

    created Gruntfile to handle deployment of entire site

---
+++ Gruntfile.js
@@ -0,0 +1,58 @@
+/*jshint camelcase: false */
+'use strict';
+module.exports = function(grunt) {
+
+    // load all grunt tasks matching the `grunt-*` pattern
+    require('load-grunt-tasks')(grunt);
+
+    grunt.initConfig({
+
+        // deploy via rsync
+        deploy: {
+            options: {
+                src: "./",
+                args: ["--verbose"],
+                exclude: [
+                  '.git*', 
+                  'node_modules', 
+                  '.sass-cache', 
+                  'Gruntfile.js', 
+                  'package.json', 
+                  '.DS_Store', 
+                  'README.md', 
+                  'config.rb', 
+                  '.jshintrc', 
+                  '.bowerrc', 
+                  '.idea', 
+                  '.vagrant', 
+                  '.yeopress', 
+                  '*.sql', 
+                  'local-config.php', 
+                  'puphpet', 
+                  'Vagrantfile',
+                  'html'
+                ],
+                recursive: true,
+                syncDestIgnoreExcl: true,
+                ssh: true
+            },
+            staging: {
+                 options: {
+                    dest: "~/domains/stage.southbrooklynchildrensgarden.org/html",
+                    host: "sbcg"
+                }
+            },
+            production: {
+                options: {
+                    dest: "~/path/to/theme",
+                    host: "user@host.com"
+                }
+            }
+        }
+
+    });
+
+    // rename tasks
+    grunt.renameTask('rsync', 'deploy');
+
+};
